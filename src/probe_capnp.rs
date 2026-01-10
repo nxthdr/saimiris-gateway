cbffce7d3d9d95a29693fc11690f35ba3863a9d9
matthieugouel <matthieu.gouel@protonmail.com>
2026-01-10 13:43:31 +0100

    update capnp to 0.25

--- src/probe_capnp.rs
+++ src/probe_capnp.rs
@@ -314,12 +314,13 @@ pub mod probe {
     pub fn get_annotation_types(child_index: Option<u16>, index: u32) -> ::capnp::introspect::Type {
       ::capnp::introspect::panic_invalid_annotation_indices(child_index, index)
     }
-    pub static RAW_SCHEMA: ::capnp::introspect::RawStructSchema = unsafe {::capnp::introspect::RawStructSchema::new(
-      &ENCODED_NODE,
+    pub static ARENA: ::capnp::private::arena::GeneratedCodeArena = ::capnp::private::arena::GeneratedCodeArena::new(&ENCODED_NODE);
+    pub static RAW_SCHEMA: ::capnp::introspect::RawStructSchema = ::capnp::introspect::RawStructSchema::new(
+      &ARENA,
       NONUNION_MEMBERS,
       MEMBERS_BY_DISCRIMINANT,
       MEMBERS_BY_NAME
-    )};
+    );
     pub static NONUNION_MEMBERS : &[u16] = &[0,1,2,3,4];
     pub static MEMBERS_BY_DISCRIMINANT : &[u16] = &[];
     pub static MEMBERS_BY_NAME : &[u16] = &[0,2,4,1,3];
